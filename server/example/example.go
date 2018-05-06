7c35a028eecc8b371ae95996d376bc562735512e
zhengxiaoyao0716 <1499383852@qq.com>
2018-05-06 18:11:09 +0800

    添加example的启动提示

--- server/example/example.go
+++ server/example/example.go
@@ -3,13 +3,17 @@ package main
 import (
 	"log"
 
+	"github.com/zhengxiaoyao0716/util/console"
+	"github.com/zhengxiaoyao0716/util/cout"
 	"github.com/zhengxiaoyao0716/zcli/server"
 )
 
 func main() {
-	if err := server.Start("Example", "127.0.0.1:4000"); err != nil {
+	addr := "127.0.0.1:4000"
+	if err := server.Start("Example", addr); err != nil {
 		log.Fatalln(err)
 	}
+	console.Log("Service start, use `zcli -addr %s` to connect it.", cout.Info(addr))
 	for {
 	}
 }
